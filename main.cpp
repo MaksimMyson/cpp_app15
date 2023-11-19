f2fd5916a03a13bfe0a7b4b41434f008e1132bc5
MaksimMyson <maksimmyson33@gmail.com>
2023-11-19 11:44:34 +0200

    ex2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include "stryk.h"
+#include <iostream>
 
-int main()
-{
-	
-	return 0;
+int main() {
+    Point point1 = { 1.0, 2.0 };
+    Point point2 = { 4.0, 6.0 };
+
+    std::cout << "Point 1: (" << point1.x << ", " << point1.y << ")\n";
+    std::cout << "Point 2: (" << point2.x << ", " << point2.y << ")\n";
+
+    double distance = calculateDistance(point1, point2);
+    std::cout << "Distance between the points: " << distance << std::endl;
+
+    return 0;
 }
--- stryk.h
+++ stryk.h
@@ -1,3 +1,17 @@
-#include  <iostream>
+#ifndef STRYKTYRU_H
+#define STRYKTYRU_H
 
-using namespace std;
+#include <cmath>
+
+struct Point {
+    double x;
+    double y;
+};
+
+double calculateDistance(const Point& p1, const Point& p2) {
+    double dx = p2.x - p1.x;
+    double dy = p2.y - p1.y;
+    return std::sqrt(dx * dx + dy * dy);
+}
+
+#endif
